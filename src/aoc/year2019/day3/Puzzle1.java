c8b0157f3ed94ab9e352823d70d0038541b4be30
Sheldon-Git <sheldon@ki.tng.de>
2019-12-05 21:42:54 +0100

    refactoring

--- src/aoc/year2019/day3/Puzzle1.java
+++ src/aoc/year2019/day3/Puzzle1.java
@@ -2,31 +2,53 @@ package aoc.year2019.day3;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Comparator;
+import java.util.List;
 
+import data.Coordinate;
+import data.CoordinateSystem;
+import data.State;
 import data.Wire;
-import logic.Calculator;
 
 public class Puzzle1 {
+
+	private static final Comparator<Coordinate> COMPARATOR = new Comparator<Coordinate>() {
+		@Override
+		public int compare(Coordinate o1, Coordinate o2) {
+			return Integer.compare(o1.getVectorLength(), o2.getVectorLength());
+		}
+	};
 
 	public static void main(String[] args) throws Exception {
 		String name = "example2";
 		Wire wire1 = createWire(name, 1);
 		Wire wire2 = createWire(name, 2);
 
-		// wire1 = Wire.createFrom("R8,U5,L5,D3");
-		// wire2 = Wire.createFrom("U7,R6,D4,L4");
+//		wire1 = Wire.createFrom("R8,U5,L5,D3");
+//		wire2 = Wire.createFrom("U7,R6,D4,L4");
 
 		System.out.println("wire1=" + wire1);
 		System.out.println("wire2=" + wire2);
 
-		int closestDistance = Calculator.closestDistance(wire1, wire2);
-		System.out.println("closestDistance=" + closestDistance);
+		System.out.println("closestDistance: " + closestDistance(wire1, wire2));
 	}
 
 	private static Wire createWire(String name, int number) throws IOException {
 		try (InputStream input = Puzzle1.class.getResource("/" + name + "/" + number + ".wire").openStream();) {
 			return Wire.createFrom(input);
 		}
+	}
+
+	private static int closestDistance(Wire wire1, Wire wire2) {
+		CoordinateSystem cs = new CoordinateSystem();
+		cs.applyWire(wire1, State.S1);
+		cs.applyWire(wire2, State.S2);
+
+		List<Coordinate> preResult = cs.findCoordinatesWith(State.SN);
+		System.out.println("preResult=" + preResult);
+		Coordinate closestPoint = preResult.parallelStream().sorted(COMPARATOR).findFirst().get();
+		System.out.println("closestPoint=" + closestPoint);
+		return closestPoint.getVectorLength();
 	}
 
 }
