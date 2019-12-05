9284a5939f96c69bbc5bfb938b309422e7cd1169
Sheldon-Git <sheldon@ki.tng.de>
2019-12-06 00:02:15 +0100

    implement day 4 puzzle 1

--- src/aoc/year2019/day4/Puzzle.java
+++ src/aoc/year2019/day4/Puzzle.java
@@ -15,19 +15,37 @@ public class Puzzle {
 		System.out.println("end");
 	}
 
-	private static boolean isValid(int i) {
-		boolean has2SameAdjacentDigits = has2SameAdjacentDigits(i);
-		boolean hasNoDecreasingDigits = !hasDecreasingDigits(i);
+	private static boolean isValid(final int number) {
+		boolean has2SameAdjacentDigits = has2SameAdjacentDigits(number);
+		boolean hasNoDecreasingDigits = !hasDecreasingDigits(number);
 		return has2SameAdjacentDigits && hasNoDecreasingDigits;
 	}
 
-	private static boolean hasDecreasingDigits(int i) {
-		// TODO Auto-generated method stub
+	private static boolean hasDecreasingDigits(final int number) {
+		int currentNumber = number;
+		int previousDigit = number % 10;
+		while (currentNumber > 0) {
+			int digit = currentNumber % 10;
+			if (digit < previousDigit) {
+				return true;
+			}
+			previousDigit = digit;
+			currentNumber = currentNumber / 10;
+		}
 		return false;
 	}
 
-	private static boolean has2SameAdjacentDigits(int i) {
-		// TODO Auto-generated method stub
+	private static boolean has2SameAdjacentDigits(final int number) {
+		int previousDigit = number % 10;
+		int currentNumber = number / 10;
+		while (currentNumber > 0) {
+			int digit = currentNumber % 10;
+			if (digit == previousDigit) {
+				return true;
+			}
+			previousDigit = digit;
+			currentNumber = currentNumber / 10;
+		}
 		return false;
 	}
 
