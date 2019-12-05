8f4983cf3ba9552edd0bf75458157f3b8d034518
Sheldon-Git <sheldon@ki.tng.de>
2019-12-05 21:02:06 +0100

    refactoring

--- src/data/CoordinateSystem.java
+++ src/data/CoordinateSystem.java
@@ -90,4 +90,70 @@ public class CoordinateSystem {
 		return result;
 	}
 
+	public void applyWire(Wire wire, State updateState) {
+		int x = 0;
+		int y = 0;
+		for (Vector vector : wire.getPath()) {
+			Coordinate point1;
+			Coordinate point2;
+			switch (vector.direction) {
+			case UP:
+				point1 = Coordinate.create(x, y + 1);
+				point2 = Coordinate.create(x, y + vector.length);
+				y += vector.length;
+				break;
+			case RIGHT:
+				point1 = Coordinate.create(x + 1, y);
+				point2 = Coordinate.create(x + vector.length, y);
+				x += vector.length;
+				break;
+			case DOWN:
+				point1 = Coordinate.create(x, y - 1);
+				point2 = Coordinate.create(x, y - vector.length);
+				y -= vector.length;
+				break;
+			case LEFT:
+				point1 = Coordinate.create(x - 1, y);
+				point2 = Coordinate.create(x - vector.length, y);
+				x -= vector.length;
+				break;
+			default:
+				throw new IllegalArgumentException("direction not supported: " + vector.direction);
+			}
+			applyVector(point1, point2, updateState);
+		}
+	}
+
+	private void applyVector(Coordinate point1, Coordinate point2, State updateState) {
+		if (point1.x != point2.x && point1.y != point2.y) {
+			throw new IllegalStateException("point1=" + point1 + ";point2=" + point2);
+		}
+		if (point1.x == point2.x) {
+			final int x = point1.x;
+			if (point1.y < point2.y) {
+				for (int y = point1.y; y <= point2.y; y++) {
+					updateStateAt(x, y, updateState);
+				}
+			}
+			if (point1.y > point2.y) {
+				for (int y = point1.y; y >= point2.y; y--) {
+					updateStateAt(x, y, updateState);
+				}
+			}
+		}
+		if (point1.y == point2.y) {
+			final int y = point1.y;
+			if (point1.x < point2.x) {
+				for (int x = point1.x; x <= point2.x; x++) {
+					updateStateAt(x, y, updateState);
+				}
+			}
+			if (point1.x > point2.x) {
+				for (int x = point1.x; x >= point2.x; x--) {
+					updateStateAt(x, y, updateState);
+				}
+			}
+		}
+	}
+
 }
--- src/logic/Calculator.java
+++ src/logic/Calculator.java
@@ -6,7 +6,6 @@ import java.util.List;
 import data.Coordinate;
 import data.CoordinateSystem;
 import data.State;
-import data.Vector;
 import data.Wire;
 
 public class Calculator {
@@ -18,91 +17,19 @@ public class Calculator {
 		}
 	};
 
-	private final CoordinateSystem cs;
-
-	public Calculator() {
+	public static int closestDistance(Wire wire1, Wire wire2) {
 		// TODO determine CoordinateSystem constructor argument automatically by lengths of wires
 		// TODO or just use Map instead of array
-		cs = new CoordinateSystem(20);
-	}
-
-	public static int closestDistance(Wire wire1, Wire wire2) {
-		Calculator calc = new Calculator();
-		calc.applyWire(wire1, State.S1);
-		calc.applyWire(wire2, State.S2);
+		CoordinateSystem cs = new CoordinateSystem(20);
+		cs.applyWire(wire1, State.S1);
+		cs.applyWire(wire2, State.S2);
 		// calc.cs.print();
 
-		List<Coordinate> preResult = calc.cs.findCoordinatesWith(State.SN);
+		List<Coordinate> preResult = cs.findCoordinatesWith(State.SN);
 		System.out.println("preResult=" + preResult);
 		Coordinate closestPoint = preResult.parallelStream().sorted(COMPARATOR).findFirst().get();
 		System.out.println("closestPoint=" + closestPoint);
 		return closestPoint.getVectorLength();
-	}
-
-	private void applyWire(Wire wire, State updateState) {
-		int x = 0;
-		int y = 0;
-		for (Vector vector : wire.getPath()) {
-			Coordinate point1;
-			Coordinate point2;
-			switch (vector.direction) {
-			case UP:
-				point1 = Coordinate.create(x, y + 1);
-				point2 = Coordinate.create(x, y + vector.length);
-				y += vector.length;
-				break;
-			case RIGHT:
-				point1 = Coordinate.create(x + 1, y);
-				point2 = Coordinate.create(x + vector.length, y);
-				x += vector.length;
-				break;
-			case DOWN:
-				point1 = Coordinate.create(x, y - 1);
-				point2 = Coordinate.create(x, y - vector.length);
-				y -= vector.length;
-				break;
-			case LEFT:
-				point1 = Coordinate.create(x - 1, y);
-				point2 = Coordinate.create(x - vector.length, y);
-				x -= vector.length;
-				break;
-			default:
-				throw new IllegalArgumentException("direction not supported: " + vector.direction);
-			}
-			applyVector(point1, point2, updateState);
-		}
-	}
-
-	private void applyVector(Coordinate point1, Coordinate point2, State updateState) {
-		if (point1.x != point2.x && point1.y != point2.y) {
-			throw new IllegalStateException("point1=" + point1 + ";point2=" + point2);
-		}
-		if (point1.x == point2.x) {
-			final int x = point1.x;
-			if (point1.y < point2.y) {
-				for (int y = point1.y; y <= point2.y; y++) {
-					cs.updateStateAt(x, y, updateState);
-				}
-			}
-			if (point1.y > point2.y) {
-				for (int y = point1.y; y >= point2.y; y--) {
-					cs.updateStateAt(x, y, updateState);
-				}
-			}
-		}
-		if (point1.y == point2.y) {
-			final int y = point1.y;
-			if (point1.x < point2.x) {
-				for (int x = point1.x; x <= point2.x; x++) {
-					cs.updateStateAt(x, y, updateState);
-				}
-			}
-			if (point1.x > point2.x) {
-				for (int x = point1.x; x >= point2.x; x--) {
-					cs.updateStateAt(x, y, updateState);
-				}
-			}
-		}
 	}
 
 }
