6517f6bb80f0c26804490c1d82f031ff5750793b
Sheldon-Git <sheldon@ki.tng.de>
2019-12-06 00:31:08 +0100

    add day 4 puzzle 2

---
+++ src/aoc/year2019/day4/Puzzle2.java
@@ -0,0 +1,47 @@
+package aoc.year2019.day4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Puzzle2 {
+
+	private static final int MIN = 234208;
+	private static final int MAX = 765869;
+
+	public static void main(String[] args) {
+		System.out.println(hasExactly2SameAdjacentDigits(123455));
+		System.exit(1);
+		List<Integer> result = new ArrayList<>();
+		for (int i = MIN; i <= MAX; i++) {
+			if (isValid(i)) {
+				result.add(i);
+			}
+		}
+		System.out.println(result.size());
+	}
+
+	private static boolean isValid(final int number) {
+		boolean has2SameAdjacentDigits = hasExactly2SameAdjacentDigits(number);
+		boolean hasNoDecreasingDigits = !hasDecreasingDigits(number);
+		return has2SameAdjacentDigits && hasNoDecreasingDigits;
+	}
+
+	private static boolean hasExactly2SameAdjacentDigits(final int number) {
+		// TODO implement and test
+	}
+
+	private static boolean hasDecreasingDigits(final int number) {
+		int currentNumber = number;
+		int previousDigit = number % 10;
+		while (currentNumber > 0) {
+			int digit = currentNumber % 10;
+			if (digit < previousDigit) {
+				return true;
+			}
+			previousDigit = digit;
+			currentNumber = currentNumber / 10;
+		}
+		return false;
+	}
+
+}
