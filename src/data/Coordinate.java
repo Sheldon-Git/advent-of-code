0355bb3999446966265865f46f4dd96d7855d534
Sheldon-Git <sheldon@ki.tng.de>
2019-12-05 19:46:35 +0100

    add Coordinate

---
+++ src/data/Coordinate.java
@@ -0,0 +1,17 @@
+package data;
+
+public class Coordinate {
+
+	public final int x;
+	public final int y;
+
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public static Coordinate create(int x, int y) {
+		return new Coordinate(x, y);
+	}
+
+}
